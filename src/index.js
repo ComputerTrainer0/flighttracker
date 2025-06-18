2197b23ec26c57cf059fdb4b926b37685e9372c1
venkat <wiperoftears@gmail.com>
2025-06-18 21:23:51 +0000

    Add package.json and React setup

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import FlightTracker from './FlightTracker';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+	  <React.StrictMode>
+	    <FlightTracker />
+	  </React.StrictMode>
+);
